8e56c2e07b05a850fb2a53801dfac70c36408ecf
jesjo430 <jesper_jonsson@hotmail.se>
2017-01-24 17:17:20 +0100

    Forgot some files, lul

---
+++ linuxpintos/src/examples/create.d
@@ -0,0 +1,1 @@
+create.o: create.c ../lib/user/syscall.h ../lib/stdbool.h ../lib/debug.h
--- linuxpintos/src/userprog/syscall.h
+++ linuxpintos/src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include <stdbool.h>
 
 void syscall_init (void);
-
+static void halt(void);
+static bool create(const char *file, unsigned initial_size);
 #endif /* userprog/syscall.h */
