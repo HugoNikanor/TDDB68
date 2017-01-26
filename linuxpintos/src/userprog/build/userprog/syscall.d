b84129fd9f6b66b16e5880dbe573a9bb4efa8e4f
jesjo430 <jesper_jonsson@hotmail.se>
2017-01-26 10:00:37 +0100

    Started working on FD

--- linuxpintos/src/userprog/build/userprog/syscall.d
+++ linuxpintos/src/userprog/build/userprog/syscall.d
@@ -3,4 +3,4 @@ userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
  ../../lib/stdarg.h ../../lib/stddef.h ../../lib/stdint.h \
  ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
  ../../threads/interrupt.h ../../threads/thread.h ../../lib/kernel/list.h \
- ../../threads/init.h ../../filesys/filesys.h ../../filesys/off_t.h
+ ../../filesys/filesys.h ../../filesys/off_t.h ../../threads/init.h
